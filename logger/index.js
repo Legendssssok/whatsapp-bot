005ab47a80e025379fee6afb79f1d7a6346320fd
Legendssssok <143305583+Legendssssok@users.noreply.github.com>
2024-08-29 22:50:34 +0530

    Create index.js

---
+++ logger/index.js
@@ -0,0 +1,13 @@
+module.exports = async function logger(client, text) {
+    try {
+      await client.sendMessage(client.info.wid._serialized, text);
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  };
+  
+  // Example:
+  // await logger(client, "message");
+  
